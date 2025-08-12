20bd7c42f7e34c40a28841ee795da01165de769a
Qiong Zhou Huang <qiong@phonic.co>
2025-08-12 23:34:53 +0000

    fix types

--- src/api/types/overrides.ts
+++ src/api/types/overrides.ts
@@ -1,59 +1,7 @@
+import type { Conversation } from "./Conversation";
+
 type PhonicTool = "send_dtmf_tone" | "end_conversation" | (string & {});
 type ISODateTime = `${string}Z`;
-
-type TaskStatus = "pending" | "completed" | "failed";
-
-type TaskResult = {
-    name: string;
-    description: string;
-    status: TaskStatus;
-    commentary: string | null;
-};
-
-type TaskResults = {
-    results: Array<TaskResult>;
-};
-
-type ConversationItem =
-    | {
-        role: "user";
-        item_idx: number;
-        text: string;
-        duration_ms: number;
-        started_at: string;
-    }
-    | {
-        role: "assistant";
-        item_idx: number;
-        text: string;
-        voice_id: string;
-        system_prompt: string;
-        audio_speed: number;
-        duration_ms: number;
-        started_at: string;
-    };
-
-export type Conversation = {
-    id: string;
-    external_id: string | null;
-    workspace: string;
-    agent: {
-        id: string;
-        name: string;
-    } | null;
-    model: string;
-    welcome_message: string | null;
-    input_format: "pcm_44100" | "mulaw_8000";
-    output_format: "pcm_44100" | "mulaw_8000";
-    live_transcript: string;
-    post_call_transcript: string | null;
-    audio_url: string | null;
-    duration_ms: number;
-    task_results: TaskResults;
-    started_at: ISODateTime;
-    ended_at: ISODateTime | null;
-    items: Array<ConversationItem>;
-};
 
 export type PhonicConfigurationEndpointRequestPayload = {
     project: {
