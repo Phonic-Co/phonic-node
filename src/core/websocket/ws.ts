71a69e5b36beb001e4dc590b4f32c3c55a67d89b
Qiong Zhou Huang <qiong@phonic.co>
2025-08-28 19:36:03 +0000

    fix

--- src/core/websocket/ws.ts
+++ src/core/websocket/ws.ts
@@ -28,7 +28,7 @@ export declare namespace ReconnectingWebSocket {
         url: string;
         protocols?: string | string[];
         options?: ReconnectingWebSocket.Options;
-        headers?: Record<string, string>;
+        headers?: Record<string, any>;
         queryParameters?: Record<string, string | string[] | object | object[] | null | undefined>;
     }
 
